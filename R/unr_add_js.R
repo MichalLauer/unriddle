37cc2f1f6e7b1fb77ab84ef9820520ca1242b303
Michal Lauer NTBK <michal.lauer.25@gmail.com>
2024-06-10 13:57:10 +0200

    Fix for multiple unriddles

--- R/unr_add_js.R
+++ R/unr_add_js.R
@@ -1,28 +1,50 @@
-unr_add_js <- function(code) {
-  header <- "```{js aaa}"
+unr_add_js <- function(code, name) {
+  header <- paste0("```{js ", name, "-js}")
+  # body <- c(
+  #   "let mainDivs = document.querySelectorAll('div[data-unriddle=\"true\"]');",
+  #   "mainDivs.forEach(mainDiv => {",
+  #   "  mainDiv.removeChild(mainDiv.querySelector('.cell-output-display'))",
+  #   "  let sectionParent = mainDiv.closest('section');",
+  #   "  let childDivs = Array.from(mainDiv.querySelectorAll('.columns'));",
+  #   "  let n = childDivs.length;",
+  #   "  for (let i = 0; i < n; i++) {",
+  #   "    let sectionClone = sectionParent.cloneNode(true);",
+  #   "    let mainDivClone = sectionClone.querySelector('div[data-unriddle=\"true\"]');",
+  #   "    let childDivsClone = mainDivClone.querySelectorAll('.columns')",
+  #   "    Array.from(childDivsClone).forEach((child, index) => {",
+  #   "      if (index !== i) {",
+  #   "        mainDivClone.removeChild(child);",
+  #   "      }",
+  #   "    });",
+  #   "    if (sectionClone.id) {",
+  #   "      sectionClone.id = sectionClone.id + (i + 1);",
+  #   "    }",
+  #   "    sectionParent.insertAdjacentElement('beforebegin', sectionClone);",
+  #   "  }",
+  #   "  sectionParent.remove();",
+  #   "});"
+  # )
   body <- c(
-    "const mainDivs = document.querySelectorAll('div[data-unriddle=\"true\"]');",
-    "mainDivs.forEach(mainDiv => {",
-    "  mainDiv.removeChild(mainDiv.querySelector('.cell-output-display'))",
-    "  const sectionParent = mainDiv.closest('section');",
-    "  const childDivs = Array.from(mainDiv.querySelectorAll('.columns'));",
-    "  const n = childDivs.length;",
-    "  for (let i = 0; i < n; i++) {",
-    "    const sectionClone = sectionParent.cloneNode(true);",
-    "    const mainDivClone = sectionClone.querySelector('div[data-unriddle=\"true\"]');",
-    "    const childDivsClone = mainDivClone.querySelectorAll('.columns')",
-    "    Array.from(childDivsClone).forEach((child, index) => {",
-    "      if (index !== i) {",
-    "        mainDivClone.removeChild(child);",
-    "      }",
-    "    });",
-    "    if (sectionClone.id) {",
-    "      sectionClone.id = sectionClone.id + (i + 1);",
+    "var mainDiv = document.currentScript.parentElement.parentElement;",
+    "mainDiv.removeChild(mainDiv.querySelector('.cell-output-display'))",
+    "var sectionParent = mainDiv.closest('section');",
+    "var childDivs = Array.from(mainDiv.querySelectorAll('.columns'));",
+    "var n = childDivs.length;",
+    "for (let i = 0; i < n; i++) {",
+    "  var sectionClone = sectionParent.cloneNode(true);",
+    "  var mainDivClone = sectionClone.querySelector('div[data-unriddle=\"true\"]');",
+    "  var childDivsClone = mainDivClone.querySelectorAll('.columns')",
+    "  Array.from(childDivsClone).forEach((child, index) => {",
+    "    if (index !== i) {",
+    "      mainDivClone.removeChild(child);",
     "    }",
-    "    sectionParent.insertAdjacentElement('beforebegin', sectionClone);",
-    "  }",
-    "  sectionParent.remove();",
-    "});"
+    "  });",
+    "  if (sectionClone.id) {",
+    "    sectionClone.id = sectionClone.id + (i + 1);",
+    "   }",
+    "   sectionParent.insertAdjacentElement('beforebegin', sectionClone);",
+    "}",
+    "sectionParent.remove();"
   )
   footer <- "```"
 
